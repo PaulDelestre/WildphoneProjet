c9ffde5d7302fe67fd444e02daa8d8bfef106b8a
dev-talbi <dev.talbi@gmail.com>
2020-12-08 16:28:14 +0100

    set environement for deploy

---
+++ config/db.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file define BD access infos.
+ *
+ * PHP version 7
+ *
+ * @category  PHP
+ * @package   PHP_SimpleMVC
+ * @author    WCS <contact@wildcodeschool.fr>
+ * @license  --
+ * @link     https://github.com/WildCodeSchool/simple-mvc
+ *
+ */
+
+define('APP_DB_USER', 'wild_phone_8528');
+define('APP_DB_PWD', 'NCoC83YoF67xrOmw87Xa');
+define('APP_DB_HOST', 'b2d712fc-6b7c-4930-85cf-9ee659a927ab.wild-phone-8528.mysql.dbs.scalingo.com:33468/wild_phone_8528?useSSL=true&verifyServerCertificate=false');
+define('APP_DB_NAME', 'wild_phone_8528');
+
